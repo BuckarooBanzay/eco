89febe76464ca4659de82d8f78863a77be9c3c92
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-10 21:33:59 +0200

    simple schematic save/load

---
+++ mods/eco_api/init.lua
@@ -0,0 +1,26 @@
+local function get_mapblock_bounds(pos)
+	local mapblock = vector.floor( vector.divide(pos, 16))
+  local min = vector.multiply(mapblock, 16)
+  local max = vector.add(min, 15)
+	return min, max
+end
+
+minetest.register_chatcommand("test", {
+	func = function(name)
+    local player = minetest.get_player_by_name(name)
+    local pos = player:get_pos()
+    local min, max = get_mapblock_bounds(pos)
+    minetest.create_schematic(min, max, nil, minetest.get_worldpath() .. "/export.mts")
+		return true
+  end
+})
+
+minetest.register_chatcommand("test2", {
+	func = function(name)
+    local player = minetest.get_player_by_name(name)
+    local pos = player:get_pos()
+    local min = get_mapblock_bounds(pos)
+    minetest.place_schematic(min, minetest.get_worldpath() .. "/export.mts", 0, {}, true, {})
+		return true
+  end
+})
