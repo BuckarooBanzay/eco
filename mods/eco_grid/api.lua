12708bf2cc323222c1e5deae1227d7ab9ce5dcf5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-11 12:00:15 +0200

    mapblock helpers

--- mods/eco_grid/api.lua
+++ mods/eco_grid/api.lua
@@ -1,14 +1,22 @@
 
 local grid = {}
 
-function eco_grid.set(pos, data)
-  local mapblock = eco_util.get_mapblock(pos)
+function eco_grid.set_mapblock(mapblock, data)
   local hash = minetest.hash_node_position(mapblock)
   grid[hash] = data
 end
 
-function eco_grid.get(pos)
+function eco_grid.set(pos, data)
   local mapblock = eco_util.get_mapblock(pos)
+  eco_grid.set_mapblock(mapblock, data)
+end
+
+function eco_grid.get_mapblock(mapblock)
   local hash = minetest.hash_node_position(mapblock)
   return grid[hash]
+end
+
+function eco_grid.get(pos)
+  local mapblock = eco_util.get_mapblock(pos)
+  return eco_grid.get_mapblock(mapblock)
 end
