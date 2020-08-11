202a44e09f7fafda5eb81ac175d56f0a897e302b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-11 08:58:35 +0200

    working deserialize

--- mods/eco_serialize/deserialize.lua
+++ mods/eco_serialize/deserialize.lua
@@ -1,3 +1,54 @@
 
-function eco_serialize.deserialize(pos, filename)
+local function read_json_file(filename)
+  local file = io.open(filename,"r")
+
+  if file then
+    local json = file:read("*a")
+    return minetest.parse_json(json)
+  else
+    return nil
+  end
+end
+
+
+local function worker(ctx)
+  if not ctx.pos then
+    -- done
+    minetest.log("action", "[eco_serialize] done reading schema from " .. ctx.schema_dir)
+    return
+  end
+
+  minetest.log("action", "[eco_serialize] deserializing mapblock at position " .. dump(ctx.pos))
+
+  local mapblock = read_json_file(ctx.schema_dir .. "/mapblock_" .. ctx.mapblock_index .. ".json")
+  local metadata = read_json_file(ctx.schema_dir .. "/mapblock_" .. ctx.mapblock_index .. ".metadata.json")
+
+  eco_serialize.deserialize_part(ctx.pos, ctx.manifest.node_mapping, mapblock, metadata)
+
+  -- shift context
+  ctx.mapblock_index = ctx.mapblock_index + 1
+  ctx.pos = eco_serialize.iterator_next(ctx.min, ctx.max, ctx.pos)
+
+  minetest.after(0.5, worker, ctx)
+end
+
+
+function eco_serialize.deserialize(pos, schema_dir)
+  local manifest = read_json_file(schema_dir .. "/manifest.json")
+  local min = eco_util.get_mapblock_bounds(pos)
+
+  local ctx = {
+    manifest = manifest,
+    schema_dir = schema_dir,
+    min = min,
+    max = {
+      x = min.x + manifest.max_x,
+      y = min.y + manifest.max_y,
+      z = min.z + manifest.max_z
+    },
+    mapblock_index = 1,
+    pos = min
+  }
+
+  minetest.after(0, worker, ctx)
 end
---
+++ mods/eco_serialize/deserialize_part.lua
@@ -0,0 +1,69 @@
+local air_content_id = minetest.get_content_id("air")
+
+-- local nodename->id cache
+local local_nodename_to_id_mapping = {} -- name -> id
+
+function eco_serialize.deserialize_part(pos1, node_mapping, data, metadata)
+  local foreign_nodeid_to_name_mapping = {} -- id -> name
+  for k, v in pairs(node_mapping) do
+    foreign_nodeid_to_name_mapping[v] = k
+  end
+
+  local pos2 = vector.add(pos1, 15)
+
+  local manip = minetest.get_voxel_manip()
+  local e1, e2 = manip:read_from_map(pos1, pos2)
+  local area = VoxelArea:new({MinEdge=e1, MaxEdge=e2})
+
+  for i, node_id in ipairs(data.node_ids) do
+    local node_name = foreign_nodeid_to_name_mapping[node_id]
+    local local_node_id = local_nodename_to_id_mapping[node_name]
+    if not local_node_id then
+      if minetest.registered_nodes[node_name] then
+        -- node is locally available
+        local_node_id = minetest.get_content_id(node_name)
+      else
+        -- node is not available here
+        -- TODO: make replacements configurable
+        local_node_id = air_content_id
+      end
+      local_nodename_to_id_mapping[node_name] = local_node_id
+
+    end
+
+    data.node_ids[i] = local_node_id
+  end
+
+  local node_data = manip:get_data()
+  local param1 = manip:get_light_data()
+  local param2 = manip:get_param2_data()
+
+  local j = 1
+  for x=pos1.x,pos2.x do
+  for y=pos1.y,pos2.y do
+  for z=pos1.z,pos2.z do
+    local i = area:index(x,y,z)
+    node_data[i] = data.node_ids[j]
+    param1[i] = data.param1[j]
+    param2[i] = data.param2[j]
+    j = j + 1
+  end
+  end
+  end
+
+  manip:set_data(node_data)
+  manip:set_light_data(param1)
+  manip:set_param2_data(param2)
+  manip:write_to_map()
+
+  -- deserialize metadata
+  if metadata and metadata.meta then
+    for pos_str, md in pairs(metadata.meta) do
+      local relative_pos = minetest.string_to_pos(pos_str)
+      local absolute_pos = vector.add(pos1, relative_pos)
+      minetest.get_meta(absolute_pos):from_table(md)
+    end
+  end
+
+
+end
