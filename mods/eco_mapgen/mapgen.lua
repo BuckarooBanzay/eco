c06aa3eaadee6bce59b6ada5c7d6e5035d7228c3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-12 11:08:57 +0200

    slope check stub

--- mods/eco_mapgen/mapgen.lua
+++ mods/eco_mapgen/mapgen.lua
@@ -12,9 +12,22 @@ minetest.register_on_generated(function(minp, maxp)
 	for x=min_mapblock.x,max_mapblock.x do
 		local height = eco_mapgen.get_mapblock_height({ x=x, z=z })
 
+		local xplus_height = eco_mapgen.get_mapblock_height({ x=x+1, z=z })
+		local xminus_height = eco_mapgen.get_mapblock_height({ x=x-1, z=z })
+		local zplus_height = eco_mapgen.get_mapblock_height({ x=x, z=z+1 })
+		local zminus_height = eco_mapgen.get_mapblock_height({ x=x, z=z-1 })
+
+		local xplus_higher = xplus_height > height
+		local xminus_higher = xminus_height > height
+		local zplus_higher = zplus_height > height
+		local zminus_higher = zminus_height > height
+
 		for y=min_mapblock.y,max_mapblock.y do
 
 			if y == height then
+				-- solid mapblock here
+				-- TODO: check adjecent height and place slope
+
 				local mapblock = { x=x, y=y, z=z }
 				local pos = eco_util.get_mapblock_bounds_from_mapblock(mapblock)
 				eco_serialize.deserialize(pos, MP .. "/schematics/grass_flat", true)
