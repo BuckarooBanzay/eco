4783107c47445451e66556fdd10337dc86b27c75
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-13 07:33:43 +0200

    add get_height cache

--- mods/eco_mapgen/height.lua
+++ mods/eco_mapgen/height.lua
@@ -7,8 +7,15 @@ local height_params = {
 	persist = 0.5
 }
 
--- TODO: cache this per chunk
+-- TODO: check memory usage after long mapgen activity
+local cache = {}
+
 function eco_mapgen.get_mapblock_height(mapblock)
+	local hash = minetest.hash_node_position({ x=mapblock.x, y=0, z=mapblock.z })
+	if cache[hash] ~= nil then
+		return cache[hash]
+	end
+
   local map_lengths_xyz = {x=1, y=1, z=1}
   local height_perlin = minetest.get_perlin_map(height_params, map_lengths_xyz)
   local height_perlin_map = {}
@@ -16,6 +23,8 @@ function eco_mapgen.get_mapblock_height(mapblock)
 
   local height_perlin_factor = math.abs( height_perlin_map[1] )
   local height = math.floor(height_perlin_factor * 5)
+
+	cache[hash] = height
 
   return height
 end
