cf1bd0caf2916eb71cc42833f94e3820ad87d5a1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-14 09:50:17 +0200

    add house types

--- mods/eco_buildings/init.lua
+++ mods/eco_buildings/init.lua
@@ -1,8 +1,28 @@
 local MP = minetest.get_modpath("eco_buildings")
 
 eco_api.register_building({
-  key = "eco_buildings:simple_house",
-  name = "Simple house",
+  key = "eco_buildings:simple_house_low_density",
+  name = "Simple house with low density population",
   cost = 25000,
-  schema_dir = MP .. "/schematics/simple_house"
+  schemas = {
+    MP .. "/schematics/simple_house_low_density"
+  }
+})
+
+eco_api.register_building({
+  key = "eco_buildings:simple_house_average_density",
+  name = "Simple house with average density population",
+  cost = 50000,
+  schemas = {
+    MP .. "/schematics/simple_house_average_density"
+  }
+})
+
+eco_api.register_building({
+  key = "eco_buildings:simple_house_high_density",
+  name = "Simple house with high density population",
+  cost = 75000,
+  schemas = {
+    MP .. "/schematics/simple_house_high_density"
+  }
 })
