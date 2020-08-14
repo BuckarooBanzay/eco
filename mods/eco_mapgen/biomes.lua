01d10e9140f05e44e429265ae57a76909766a0b3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-14 20:02:34 +0200

    working biome api

--- mods/eco_mapgen/biomes.lua
+++ mods/eco_mapgen/biomes.lua
@@ -1,19 +1,35 @@
--- TODO match priority
+-- TODO priority / unregistering
+local MP = minetest.get_modpath("eco_mapgen")
 
 eco_mapgen.register_biome({
   key = "water",
   match = function(mapblock)
-    return mapblock.y <= eco_mapgen.get_water_height()
+    local is_in_water = mapblock.y <= eco_mapgen.get_water_height()
+    local map_height = eco_mapgen.get_mapblock_height(mapblock)
+
+    return mapblock.y == map_height and is_in_water
   end,
-  generate = function(mapblock)
-  end
+  schemas = {
+    flat = MP .. "/schematics/water_flat",
+    slope = MP .. "/schematics/water_slope",
+    slope_inner = MP .."/schematics/water_slope_inner_corner",
+    slope_outer = MP .."/schematics/water_slope_outer_corner",
+    empty = MP .. "/schematics/water_full"
+  }
 })
 
 eco_mapgen.register_biome({
   key = "grass",
   match = function(mapblock)
-    return mapblock.y > eco_mapgen.get_water_height()
+    local is_in_water = mapblock.y <= eco_mapgen.get_water_height()
+    local map_height = eco_mapgen.get_mapblock_height(mapblock)
+
+    return mapblock.y == map_height and not is_in_water
   end,
-  generate = function(mapblock)
-  end
+  schemas = {
+    flat = MP .. "/schematics/grass_flat",
+    slope = MP .. "/schematics/grass_slope",
+    slope_inner = MP .."/schematics/grass_slope_inner_corner",
+    slope_outer = MP .."/schematics/grass_slope_outer_corner"
+  }
 })
--- mods/eco_mapgen/mapgen.lua
+++ mods/eco_mapgen/mapgen.lua
@@ -1,4 +1,79 @@
-local MP = minetest.get_modpath("eco_mapgen")
+
+local function place_biome_mapblock(mapblock, biome)
+	local info = eco_mapgen.get_info(mapblock)
+	local pos = eco_util.get_mapblock_bounds_from_mapblock(mapblock)
+
+	if info.type == "flat" and biome.schemas.flat then
+		eco_serialize.deserialize(pos, biome.schemas.flat, {
+			use_cache = true
+		})
+
+	elseif info.type == "slope" and biome.schemas.slope then
+		-- slope looks into z+ direction
+		local rotate = nil
+		if info.direction == "z-" then
+			rotate = { axis = "y", angle = 180 }
+		elseif info.direction == "x+" then
+			rotate = { axis = "y", angle = 90 }
+		elseif info.direction == "x-" then
+			rotate = { axis = "y", angle = 270 }
+		end
+
+		eco_serialize.deserialize(pos, biome.schemas.slope, {
+			use_cache = true,
+			sync = true,
+			transform = {
+				rotate = rotate
+			}
+		})
+
+	elseif info.type == "slope_inner" and biome.schemas.slope_inner then
+		-- slope looks into x-z+ direction
+		local rotate = nil
+		if info.direction == "x-z-" then
+			rotate = { axis = "y", angle = 270 }
+		elseif info.direction == "x+z-" then
+			rotate = { axis = "y", angle = 180 }
+		elseif info.direction == "x+z+" then
+			rotate = { axis = "y", angle = 90 }
+		end
+
+		eco_serialize.deserialize(pos, biome.schemas.slope_inner, {
+			use_cache = true,
+			sync = true,
+			transform = {
+				rotate = rotate
+			}
+		})
+
+	elseif info.type == "slope_outer" and biome.schemas.slope_outer then
+		-- slope looks into x-z+ direction
+		local rotate = nil
+		if info.direction == "x-z-" then
+			rotate = { axis = "y", angle = 270 }
+		elseif info.direction == "x+z-" then
+			rotate = { axis = "y", angle = 180 }
+		elseif info.direction == "x+z+" then
+			rotate = { axis = "y", angle = 90 }
+		end
+
+		eco_serialize.deserialize(pos, biome.schemas.slope_outer, {
+			use_cache = true,
+			sync = true,
+			transform = {
+				rotate = rotate
+			}
+		})
+
+	elseif info.type == "none" and biome.schemas.empty then
+		eco_serialize.deserialize(pos, biome.schemas.empty, {
+			use_cache = true,
+			sync = true
+		})
+
+	end
+
+end
 
 minetest.register_on_generated(function(minp, maxp)
 
@@ -13,78 +88,17 @@ minetest.register_on_generated(function(minp, maxp)
 	for y=min_mapblock.y,max_mapblock.y do
 
 		local mapblock = { x=x, y=y, z=z }
-		local info = eco_mapgen.get_info(mapblock)
-		local pos = eco_util.get_mapblock_bounds_from_mapblock(mapblock)
 
-		local schema_prefix = MP .. "/schematics/" .. info.biome .. "_"
-
-		if info.type == "flat" then
-			eco_serialize.deserialize(pos, schema_prefix .. "flat", {
-				use_cache = true
-			})
-
-		elseif info.type == "slope" then
-			-- slope looks into z+ direction
-			local rotate = nil
-			if info.direction == "z-" then
-				rotate = { axis = "y", angle = 180 }
-			elseif info.direction == "x+" then
-				rotate = { axis = "y", angle = 90 }
-			elseif info.direction == "x-" then
-				rotate = { axis = "y", angle = 270 }
+		local biome = nil
+		for _, biome_def in pairs(eco_mapgen.get_biomes()) do
+			if biome_def.match(mapblock) then
+				biome = biome_def
+				break
 			end
+		end
 
-			eco_serialize.deserialize(pos, schema_prefix .. "slope", {
-				use_cache = true,
-				sync = true,
-				transform = {
-					rotate = rotate
-				}
-			})
-
-		elseif info.type == "slope_inner" then
-			-- slope looks into x-z+ direction
-			local rotate = nil
-			if info.direction == "x-z-" then
-				rotate = { axis = "y", angle = 270 }
-			elseif info.direction == "x+z-" then
-				rotate = { axis = "y", angle = 180 }
-			elseif info.direction == "x+z+" then
-				rotate = { axis = "y", angle = 90 }
-			end
-
-			eco_serialize.deserialize(pos, schema_prefix .. "slope_inner_corner", {
-				use_cache = true,
-				sync = true,
-				transform = {
-					rotate = rotate
-				}
-			})
-
-		elseif info.type == "slope_outer" then
-			-- slope looks into x-z+ direction
-			local rotate = nil
-			if info.direction == "x-z-" then
-				rotate = { axis = "y", angle = 270 }
-			elseif info.direction == "x+z-" then
-				rotate = { axis = "y", angle = 180 }
-			elseif info.direction == "x+z+" then
-				rotate = { axis = "y", angle = 90 }
-			end
-
-			eco_serialize.deserialize(pos, schema_prefix .. "slope_outer_corner", {
-				use_cache = true,
-				sync = true,
-				transform = {
-					rotate = rotate
-				}
-			})
-		elseif info.type == "none" and info.biome == "water" then
-			eco_serialize.deserialize(pos, MP .. "/schematics/water_full", {
-				use_cache = true,
-				sync = true
-			})
-
+		if biome then
+			place_biome_mapblock(mapblock, biome)
 		end
 
 		end --y
