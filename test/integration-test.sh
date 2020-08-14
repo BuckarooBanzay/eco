a76d107bf5b179415431d1ff247d09f1ca764d84
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-14 15:25:39 +0200

    integration test fix

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -8,7 +8,7 @@ MTDIR=/tmp/mt
 WORLDDIR=${MTDIR}/worlds/world
 
 cat <<EOF > ${CFG}
-# empty
+default_game = eco
 EOF
 
 mkdir -p ${WORLDDIR}
@@ -16,8 +16,8 @@ chmod 777 ${MTDIR} -R
 docker run --rm -i \
 	-v ${CFG}:/etc/minetest/minetest.conf:ro \
 	-v ${MTDIR}:/var/lib/minetest/.minetest \
-	-v $(pwd)/:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange \
-  -v $(pwd)/test/test_mod/:/var/lib/minetest/.minetest/worlds/world/worldmods/blockexchange_test \
+	-v $(pwd)/:/var/lib/minetest/.minetest/games/eco \
+  -v $(pwd)/test/test_mod/:/var/lib/minetest/.minetest/worlds/world/worldmods/eco_test \
   --network host \
 	registry.gitlab.com/minetest/minetest/server:5.2.0
 
