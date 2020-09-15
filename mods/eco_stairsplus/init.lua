394c711be4d787431380e01b7e165dfb2e49d34a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-09-15 22:16:36 +0200

    remove mypaths / change schematics to builtin slopes

--- mods/eco_stairsplus/init.lua
+++ mods/eco_stairsplus/init.lua
@@ -5,6 +5,12 @@ stairsplus:register_all("eco_stairsplus", "dirt", "default:dirt", {
   tiles={"default_dirt.png"},
 })
 
+stairsplus:register_all("eco_stairsplus", "dirt_with_grass", "default:dirt_with_grass", {
+  description="Dirt with grass",
+  groups={cracky=1, not_in_creative_inventory=1},
+  tiles={"default_grass.png"},
+})
+
 stairsplus:register_all("eco_stairsplus", "gravel", "default:gravel", {
   description="Gravel",
   groups={cracky=1, not_in_creative_inventory=1},
