f1a614712bc27491df1f5607a50654421fd951d8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-09-15 21:53:04 +0200

    add eco stairsplus with dirt and gravel registration

---
+++ mods/eco_stairsplus/init.lua
@@ -0,0 +1,12 @@
+
+stairsplus:register_all("eco_stairsplus", "dirt", "default:dirt", {
+  description="Dirt",
+  groups={cracky=1, not_in_creative_inventory=1},
+  tiles={"default_dirt.png"},
+})
+
+stairsplus:register_all("eco_stairsplus", "gravel", "default:gravel", {
+  description="Gravel",
+  groups={cracky=1, not_in_creative_inventory=1},
+  tiles={"default_gravel.png"},
+})
