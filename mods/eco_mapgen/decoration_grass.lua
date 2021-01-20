564820a4dec63f416dc6d9def0e3eb481044c9c5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-20 17:09:27 +0100

    randomize decorations

--- mods/eco_mapgen/decoration_grass.lua
+++ mods/eco_mapgen/decoration_grass.lua
@@ -1,11 +1,21 @@
 local MP = minetest.get_modpath("eco_mapgen")
 
+local angles = {0, 90, 180, 270}
+
 eco_mapgen.register_decoration({
 	render = function(mapblock_pos, info, height, biome)
 		if biome and biome.name == "grass" and info.type == "flat" and height > 2 then
-			mapblock_lib.deserialize(mapblock_pos, MP .. "/schematics/decorations/grass_with_tree_1", {
+
+			local path = MP .. "/schematics/decorations/grass_with_tree_" .. math.random(4)
+			mapblock_lib.deserialize(mapblock_pos, path, {
 				use_cache = true,
-				mode = "add"
+				mode = "add",
+				transform = {
+					rotate = {
+						axis = "y",
+						angle = angles[math.random(#angles)]
+					}
+				}
 			})
 		end
 	end
