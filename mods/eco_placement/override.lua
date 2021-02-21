af7ea45516da4f9445fc6874fa3a43d3c5d621cc
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-21 19:34:45 +0100

    on_secondary_use debug

--- mods/eco_placement/override.lua
+++ mods/eco_placement/override.lua
@@ -10,3 +10,17 @@ minetest.register_on_mods_loaded(function()
 		end
 	end
 end)
+
+minetest.override_item("", {
+	on_use = function()
+		print("on_use")
+	end,
+	on_secondary_use = function(_, player)
+		print("on_secondary_use")
+		local mapblock_pos = eco_placement.get_pointed_mapblock_pos(player)
+		if mapblock_pos then
+			local mapblock_data = mapblock_lib.get_mapblock_data(mapblock_pos)
+			print(dump(mapblock_pos), dump(mapblock_data))
+		end
+	end
+})
