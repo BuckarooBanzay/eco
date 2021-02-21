fe5abdc3db03f8409e9ca481197f9195518da293
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-21 19:39:02 +0100

    show mapblock pos

--- mods/eco_placement/override.lua
+++ mods/eco_placement/override.lua
@@ -19,6 +19,7 @@ minetest.override_item("", {
 		print("on_secondary_use")
 		local mapblock_pos = eco_placement.get_pointed_mapblock_pos(player)
 		if mapblock_pos then
+			mapblock_lib.display_mapblock(mapblock_pos, minetest.pos_to_string(mapblock_pos), 4)
 			local mapblock_data = mapblock_lib.get_mapblock_data(mapblock_pos)
 			print(dump(mapblock_pos), dump(mapblock_data))
 		end
