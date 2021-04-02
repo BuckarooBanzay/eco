db74b76b4fe9d10c5fdc78eab7f3cd2f664d891d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-04-02 12:51:18 +0200

    stone platform

--- mods/eco_buildings/init.lua
+++ mods/eco_buildings/init.lua
@@ -3,6 +3,7 @@ eco_buildings = {}
 local MP = minetest.get_modpath("eco_buildings")
 
 dofile(MP .. "/house.lua")
+dofile(MP .. "/stone_platform.lua")
 dofile(MP .. "/street.lua")
 dofile(MP .. "/pylon.lua")
 dofile(MP .. "/timber.lua")
---
+++ mods/eco_buildings/stone_platform.lua
@@ -0,0 +1,19 @@
+local MP = minetest.get_modpath("eco_buildings")
+
+minetest.register_craftitem("eco_buildings:stone_platform", {
+	description = "Stone platform",
+	inventory_image = "default_mese_crystal.png",
+	eco = {
+		place_building = "eco_buildings:stone_platform",
+		place_on = {
+			mapgen_type = {"flat"},
+			biome = {"grass", "snow"}
+		}
+	}
+})
+
+building_lib.register({
+	name = "eco_buildings:stone_platform",
+	placement = "simple",
+	schematic = MP .. "/schematics/stone_platform"
+})
