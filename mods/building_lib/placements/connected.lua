d85e44d54ef9dc09af9e52c9f3c809c8029797f7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-04-03 18:24:58 +0200

    connect by groups

--- mods/building_lib/placements/connected.lua
+++ mods/building_lib/placements/connected.lua
@@ -1,26 +1,36 @@
 
+local function connects_to_groups(groups, connects_to)
+	for group in pairs(connects_to) do
+		if groups[group] then
+			return true
+		end
+	end
+	return false
+end
 
-local function is_connected(mapblock_pos, name)
-	local building_def = building_lib.get_building_at_pos(mapblock_pos)
-	return building_def and building_def.name == name
+local function is_connected(mapblock_pos, connects_to)
+	local other_groups = building_lib.get_groups_at_pos(mapblock_pos)
+	return connects_to_groups(other_groups, connects_to)
 end
 
 
-local function place_street(name, mapblock_pos, schematics)
+local function place_street(mapblock_pos, building_def)
 	local mapblock_pos_upper = { x=mapblock_pos.x, y=mapblock_pos.y+1, z=mapblock_pos.z }
 	local mapblock_data = mapblock_lib.get_mapblock_data(mapblock_pos)
 	local mapgen_info = mapblock_data.mapgen_info
+	local schematics = building_def.schematics
+	local connects_to = building_def.connects_to
 
-	if mapgen_info.type == "flat" then
+	if not mapgen_info or mapgen_info.type == "flat" then
 		-- check connections on flat surface and one layer below
-		local xplus = is_connected(vector.add(mapblock_pos, {x=1, y=0, z=0}), name) or
-			is_connected(vector.add(mapblock_pos, {x=1, y=-1, z=0}), name)
-		local xminus = is_connected(vector.add(mapblock_pos, {x=-1, y=0, z=0}), name) or
-			is_connected(vector.add(mapblock_pos, {x=-1, y=-1, z=0}), name)
-		local zplus = is_connected(vector.add(mapblock_pos, {x=0, y=0, z=1}), name) or
-			is_connected(vector.add(mapblock_pos, {x=0, y=-1, z=1}), name)
-		local zminus = is_connected(vector.add(mapblock_pos, {x=0, y=0, z=-1}), name) or
-			is_connected(vector.add(mapblock_pos, {x=0, y=-1, z=-1}), name)
+		local xplus = is_connected(vector.add(mapblock_pos, {x=1, y=0, z=0}), connects_to) or
+			is_connected(vector.add(mapblock_pos, {x=1, y=-1, z=0}), connects_to)
+		local xminus = is_connected(vector.add(mapblock_pos, {x=-1, y=0, z=0}), connects_to) or
+			is_connected(vector.add(mapblock_pos, {x=-1, y=-1, z=0}), connects_to)
+		local zplus = is_connected(vector.add(mapblock_pos, {x=0, y=0, z=1}), connects_to) or
+			is_connected(vector.add(mapblock_pos, {x=0, y=-1, z=1}), connects_to)
+		local zminus = is_connected(vector.add(mapblock_pos, {x=0, y=0, z=-1}), connects_to) or
+			is_connected(vector.add(mapblock_pos, {x=0, y=-1, z=-1}), connects_to)
 
 		local schematic = schematics.straight
 		local options = {
@@ -141,13 +151,14 @@ local street_neighbor_updates = {
   { x=-1, y=-1, z=0 },
 }
 
-local function update_neighbor_streets(name, mapblock_pos, schematics)
+local function update_neighbor_streets(mapblock_pos, building_def)
 	-- iterate through possible connections
 	for _, offset in ipairs(street_neighbor_updates) do
 		local neighbor_mapblock = vector.add(mapblock_pos, offset)
-		local neighbor_building_def = building_lib.get_building_at_pos(neighbor_mapblock)
-		if neighbor_building_def and neighbor_building_def.name == name then
-			place_street(name, neighbor_mapblock, schematics)
+		local groups = building_lib.get_groups_at_pos(neighbor_mapblock)
+		local other_building_def = building_lib.get_building_at_pos(neighbor_mapblock)
+		if connects_to_groups(groups, building_def.connects_to) and other_building_def then
+			place_street(neighbor_mapblock, building_def)
 		end
 	end
 end
@@ -162,7 +173,7 @@ building_lib.register_placement({
 		return true
 	end,
 	place = function(mapblock_pos, building_def)
-		place_street(building_def.name, mapblock_pos, building_def.schematics)
-		update_neighbor_streets(building_def.name, mapblock_pos, building_def.schematics)
+		place_street(mapblock_pos, building_def)
+		update_neighbor_streets(mapblock_pos, building_def)
 	end
 })
--- mods/eco_buildings/street.lua
+++ mods/eco_buildings/street.lua
@@ -4,29 +4,40 @@ minetest.register_craftitem("eco_buildings:street", {
 	description = "Simple street",
 	inventory_image = "eco_buildings_inv_street.png",
 	eco = {
-		place_building = "eco_buildings:street",
-		place_on = function(mapblock_pos, _, mapgen_info)
-			local _, biome_name = eco_mapgen.get_biome(mapblock_pos)
-
-			local mapgen_matches = mapgen_info and (mapgen_info.type == "flat" or mapgen_info.type == "slope_lower")
-			local biome_matches = biome_name == "grass" or biome_name == "snow"
-			if not mapgen_matches then
-				return false, "landscape not supported"
-			elseif not biome_matches then
-				return false, "biome not supported"
-			else
-				return true
-			end
-		end
+		place_building = "eco_buildings:street"
 	}
 })
 
 building_lib.register({
 	name = "eco_buildings:street",
 	placement = "connected",
+	connects_to = {
+		street = true
+	},
 	groups = {
 		street = true
 	},
+	can_build = function(mapblock_pos)
+		-- check if there is a building below with "support" group
+		local groups_below = building_lib.get_groups_at_pos(vector.add(mapblock_pos, {x=0,y=-1,z=0}))
+		if groups_below.support then
+			return true
+		end
+
+		-- check for biome and mapgen match
+		local _, biome_name = eco_mapgen.get_biome(mapblock_pos)
+		local mapgen_info = eco_mapgen.get_info(mapblock_pos)
+
+		local mapgen_matches = mapgen_info and (mapgen_info.type == "flat" or mapgen_info.type == "slope_lower")
+		local biome_matches = biome_name == "grass" or biome_name == "snow"
+		if not mapgen_matches then
+			return false, "landscape not supported"
+		elseif not biome_matches then
+			return false, "biome not supported"
+		else
+			return true
+		end
+	end,
 	schematics = {
 		straight = MP .. "/schematics/street/street_straight",
 		all_sides = MP .. "/schematics/street/street_all_sides",
