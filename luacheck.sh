8204dbc898b3cc3714ff6cd884fb705914d65ce9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-04-06 07:45:12 +0200

    luacheck script

---
+++ luacheck.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd mods/eco_buildings && luacheck .
+cd ../eco_mapgen && luacheck .
+cd ../eco_placement && luacheck .
+cd ../eco_stairsplus && luacheck .
+cd ../builtin_disable && luacheck .
+cd ../building_lib && luacheck .
