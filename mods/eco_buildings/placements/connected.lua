2d9ff8e203298fc596b14dad614afbc408794291
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-29 07:59:44 +0200

    working slope

--- mods/eco_buildings/placements/connected.lua
+++ mods/eco_buildings/placements/connected.lua
@@ -1,7 +1,7 @@
 
 
--- local POS_SLOPE_LOWER = {x=0,y=0,z=0} -- x- on the upper side
--- local POS_SLOPE_UPPER = {x=0,y=1,z=0} -- x- on the upper side
+local POS_SLOPE_LOWER = {x=0,y=0,z=0} -- x- on the upper side
+local POS_SLOPE_UPPER = {x=0,y=1,z=0} -- x- on the upper side
 local POS_ALL_SIDES = {x=1,y=0,z=0}
 local POS_STRAIGHT = {x=2,y=0,z=0} -- x+ to x-
 local POS_THREE_SIDES = {x=3,y=0,z=0} -- x+/x-/z+
@@ -86,24 +86,68 @@ local function place_and_rotate(mapblock_pos, building_def)
 	end
 end
 
+local function place_and_rotate_slope(mapblock_pos, building_def, rotation)
+	local options = {
+		transform = {
+			rotate = {
+				angle = rotation,
+				axis = "y"
+			}
+		}
+	}
+	local catalog = mapblock_lib.get_catalog(building_def.catalog)
+	catalog:deserialize(POS_SLOPE_LOWER, mapblock_pos, options)
+	catalog:deserialize(POS_SLOPE_UPPER, vector.add(mapblock_pos, {x=0,y=1,z=0}), options)
+end
+
+local slope_update_directions = {
+	[0] = { {x=0,y=0,z=-1}, {x=0,y=1,z=1} },
+	[90] = { {x=-1,y=0,z=0}, {x=1,y=1,z=0} },
+	[180] = { {x=0,y=0,z=1}, {x=0,y=1,z=-1} },
+	[270] = { {x=1,y=0,z=0}, {x=-1,y=1,z=0} }
+}
+
 building_lib.register_placement({
 	name = "connected",
 	check = function()
 		return true
 	end,
-	get_size = function()
-		return { x=1, y=1, z=1 }
+	get_size = function(_, mapblock_pos)
+		local info = eco_mapgen.get_info(mapblock_pos)
+		if info.slope then
+			return { x=1, y=2, z=1 }
+		else
+			return { x=1, y=1, z=1 }
+		end
 	end,
 	place = function(self, mapblock_pos, building_def, callback)
-		place_and_rotate(mapblock_pos, building_def)
-		for _, dir in ipairs(neighbors) do
+		local info = eco_mapgen.get_info(mapblock_pos)
+		local neighbor_updates
+
+		if info.slope then
+			-- place a slope
+			place_and_rotate_slope(mapblock_pos, building_def, info.rotation)
+			neighbor_updates = slope_update_directions[info.rotation]
+		else
+			-- place a flat tile
+			place_and_rotate(mapblock_pos, building_def)
+			neighbor_updates = neighbors
+		end
+
+		for _, dir in ipairs(neighbor_updates) do
 			local neighbor_pos = vector.add(mapblock_pos, dir)
-			local neighbor_building_def = building_lib.get_building_at_pos(neighbor_pos)
-			if neighbor_building_def and neighbor_building_def.placement == self.name then
+			local neighbor_below_pos = vector.add(neighbor_pos, {x=0,y=-1,z=0})
+			local neighbor_below_info = eco_mapgen.get_info(neighbor_below_pos)
+			-- don't update slopes
+			if not neighbor_below_info.slope then
+				local neighbor_building_def = building_lib.get_building_at_pos(neighbor_pos)
 				-- re-place the neighboring mapblock too if it is of the same type
-				place_and_rotate(neighbor_pos, neighbor_building_def)
+				if neighbor_building_def and neighbor_building_def.placement == self.name then
+					place_and_rotate(neighbor_pos, neighbor_building_def)
+				end
 			end
 		end
+
 		callback()
 	end
 })
