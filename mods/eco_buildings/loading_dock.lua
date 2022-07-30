cf02d4c284a4b0ab70b486a825472f082b79f230
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-30 20:10:40 +0200

    loading dock

---
+++ mods/eco_buildings/loading_dock.lua
@@ -0,0 +1,13 @@
+local MP = minetest.get_modpath("eco_buildings")
+
+building_lib.register_building("eco_buildings:loading_dock", {
+	placement = "simple",
+	groups = {
+		loading_dock = true
+	},
+	ground_conditions = {
+		{ on_group = "support" },
+		{ on_solid_underground = true }
+	},
+	catalog = MP .. "/schematics/loading_dock.zip"
+})
