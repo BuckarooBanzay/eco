815e3675b447cf3ce7084f438ea7a6ae3811298a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-18 10:32:17 +0100

    cleanup

--- mods/building_lib/get_groups.lua
+++
@@ -1,9 +0,0 @@
-
-function building_lib.get_groups(mapblock_pos)
-    local building_def = building_lib.get_building_at_pos(mapblock_pos)
-    if not building_def then
-        return {}
-    else
-        return building_def.groups or {}
-    end
-end
