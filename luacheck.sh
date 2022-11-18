715da7fe0bcdee424139070a2f6768e016ddca3c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-18 10:50:10 +0100

    fix luacheck

--- luacheck.sh
+++ luacheck.sh
@@ -3,8 +3,4 @@
 set -e
 cd mods/eco_buildings && luacheck .
 cd ../eco_mapgen && luacheck .
-cd ../eco_placement && luacheck .
 cd ../eco_nodes && luacheck .
-cd ../eco_data && luacheck .
-cd ../building_lib && luacheck .
-cd ../memoize && luacheck .
