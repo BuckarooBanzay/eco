13a549afce429e39c8bfc17cb5e68984b0fadec5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-25 17:48:03 +0100

    route checks

--- mods/building_lib_transport/route.lua
+++ mods/building_lib_transport/route.lua
@@ -12,6 +12,7 @@ function building_lib_transport.create_route()
 end
 
 function Route:add(mapblock_pos)
+    -- simple sanity checks
     local building_info = building_lib.get_placed_building_info(mapblock_pos)
     if not building_info then
         return false, "no building found"
@@ -26,16 +27,30 @@ function Route:add(mapblock_pos)
         return false, "transport not supported '" .. building_info.name .. "'"
     end
 
-    -- TODO: rotate transport def
-
-    local last_rel_pos = {x=0, y=0, z=0}
-    if #self.mapblock_poslist > 0 then
-        -- calculate difference to last position
-        local last_pos = self.mapblock_poslist[#self.mapblock_poslist]
-        last_rel_pos = vector.subtract(last_pos, mapblock_pos)
+    if #self.mapblock_poslist == 0 then
+        -- first position, just add position
+        table.insert(self.mapblock_poslist, mapblock_pos)
+        return true
     end
 
-    for _, route in ipairs(building_def.transport.routes) do
+    -- calculate difference to last position
+    local last_mapblock_pos = self.mapblock_poslist[#self.mapblock_poslist]
+
+    -- more sanity checks (corner cases)
+    local last_building_info = building_lib.get_placed_building_info(last_mapblock_pos)
+    if not last_building_info then
+        return false, "previous building vanished"
+    end
+
+    local last_building_def = building_lib.get_building(last_building_info.name)
+    if not last_building_def or not last_building_def.transport then
+        return false, "previous building changed"
+    end
+
+    -- last -> current route
+    local last_to_current_mapblock_pos = vector.subtract(mapblock_pos, last_mapblock_pos)
+
+    for _, route in ipairs(last_building_def.transport.routes or {}) do
         if vector.equals(last_rel_pos, route.from) then
             -- TODO
         end
