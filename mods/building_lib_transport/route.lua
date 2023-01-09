e1ee0814ee9539cf8694a6a32a96f3a4f8404390
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-01-09 21:25:34 +0100

    route spec

--- mods/building_lib_transport/route.lua
+++ mods/building_lib_transport/route.lua
@@ -1,21 +1,36 @@
 
--- returns the connections from the end-position in that mapblock
-function building_lib_transport.get_connections(mapblock_pos, rel_end_pos)
+function building_lib_transport.get_routes(mapblock_pos, dir, type)
+    local routes = {}
+
     local info, origin = building_lib.get_placed_building_info(mapblock_pos)
     if not info then
-        return {}
+        return routes
     end
 
     local building_def = building_lib.get_building(info.name)
     if not building_def then
-        return {}
+        return routes
     end
 
     local mapblock_size = building_lib.get_building_size(building_def, info.rotation)
     local size = vector.multiply(mapblock_size, 16)
 
-    -- rotate relative end position
-    local rotated_end_pos = mapblock_lib.rotate_pos(rel_end_pos, size, info.rotation)
+    for name, route in pairs(building_def.routes or {}) do
+        local rotated_start_dir = mapblock_lib.rotate_pos(route.start_dir, size, info.rotation)
+        if vector.equals(rotated_start_dir, dir) then
+            -- direction matches, get target building
+            local target_mapblock_pos = vector.add(mapblock_pos, dir)
+            local target_info, target_origin = building_lib.get_placed_building_info(target_mapblock_pos)
+            if target_info then
+                local target_building_def = building_lib.get_building(target_info.name)
+                for target_name, target_route in pairs(target_building_def.routes or {}) do
+                    -- TODO
+                end
+                routes[name] = rotated_start_dir
+            end
+        end
+        -- TODO
+    end
 
-    -- TODO
+    return routes
 end
--- mods/building_lib_transport/route.spec.lua
+++ mods/building_lib_transport/route.spec.lua
@@ -1,9 +1,11 @@
 
 building_lib.register_building("building_lib_transport:route_straight", {
 	placement = "dummy",
-    transport = {
+    routes = {
         left = {
             type = "street",
+            start_dir = {x=-1, y=0, z=0},
+            end_dir = {x=1, y=0, z=0},
             points = {
                 {x=0, y=3, z=3},
                 {x=16, y=3, z=3}
@@ -11,6 +13,8 @@ building_lib.register_building("building_lib_transport:route_straight", {
         },
         right = {
             type = "street",
+            start_dir = {x=1, y=0, z=0},
+            end_dir = {x=-1, y=0, z=0},
             points = {
                 {x=16, y=3, z=13},
                 {x=0, y=3, z=13}
@@ -18,12 +22,6 @@ building_lib.register_building("building_lib_transport:route_straight", {
         }
     }
 })
-
-building_lib_transport.register_connection_rotated(
-    "building_lib_transport:route_straight", "left",
-    { x=1, y=0, z=0 },
-    "building_lib_transport:route_straight", "left"
-)
 
 mtt.register("route", function(callback)
 
@@ -33,8 +31,8 @@ mtt.register("route", function(callback)
     assert(building_lib.build({x=2, y=0, z=0}, playername, "building_lib_transport:route_straight"))
 
     -- end of mb1
-    local connections = building_lib_transport.get_connections({x=0, y=0, z=0}, {x=16, y=3, z=3})
-    print(dump(connections))
+    local routes = building_lib_transport.get_routes({x=0, y=0, z=0}, {x=1, y=0, z=0}, "street")
+    print(dump(routes))
 
     callback()
 end)
