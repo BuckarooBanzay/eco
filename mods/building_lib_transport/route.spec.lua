07a8d938563129a86457c2868f38b46d7b390f8d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-01-12 11:40:30 +0100

    working route system

--- mods/building_lib_transport/route.spec.lua
+++ mods/building_lib_transport/route.spec.lua
@@ -1,8 +1,9 @@
 
 building_lib.register_building("building_lib_transport:route_straight", {
-	placement = "dummy",
+    placement = "dummy",
     routes = {
-        left = {
+        {
+            name = "left",
             type = "street",
             start_dir = {x=-1, y=0, z=0},
             end_dir = {x=1, y=0, z=0},
@@ -10,8 +11,8 @@ building_lib.register_building("building_lib_transport:route_straight", {
                 {x=0, y=3, z=3},
                 {x=16, y=3, z=3}
             }
-        },
-        right = {
+        },{
+            name = "right",
             type = "street",
             start_dir = {x=1, y=0, z=0},
             end_dir = {x=-1, y=0, z=0},
@@ -23,16 +24,46 @@ building_lib.register_building("building_lib_transport:route_straight", {
     }
 })
 
-mtt.register("route", function(callback)
-
+mtt.register("route setup", function(callback)
     local playername = "singleplayer"
     assert(building_lib.build({x=0, y=0, z=0}, playername, "building_lib_transport:route_straight"))
     assert(building_lib.build({x=1, y=0, z=0}, playername, "building_lib_transport:route_straight"))
     assert(building_lib.build({x=2, y=0, z=0}, playername, "building_lib_transport:route_straight"))
 
-    -- end of mb1
-    local routes = building_lib_transport.get_routes({x=0, y=0, z=0}, {x=1, y=0, z=0}, "street")
-    print(dump(routes))
+    callback()
+end)
+
+mtt.register("get_rotated_routes", function(callback)
+    local building_def = building_lib.get_building("building_lib_transport:route_straight")
+    assert(building_def)
+    local routes = building_lib_transport.get_rotated_routes(building_def, 90)
+
+    assert(#routes == 2)
+    assert(vector.equals({x=0, y=0, z=1}, routes[1].start_dir))
+
+    callback()
+end)
+
+mtt.register("get_routes", function(callback)
+    local routes, target_mapblock, err_msg = building_lib_transport.get_routes({x=0,y=0,z=0}, "left")
+    assert(#routes == 1)
+    assert(vector.equals({x=1, y=0, z=0}, target_mapblock))
+    assert(not err_msg)
+
+    routes, target_mapblock, err_msg = building_lib_transport.get_routes({x=1,y=0,z=0}, "left")
+    assert(#routes == 1)
+    assert(vector.equals({x=2, y=0, z=0}, target_mapblock))
+    assert(not err_msg)
+
+    routes, target_mapblock, err_msg = building_lib_transport.get_routes({x=1,y=0,z=0}, "right")
+    assert(#routes == 1)
+    assert(vector.equals({x=0, y=0, z=0}, target_mapblock))
+    assert(not err_msg)
+
+    routes, target_mapblock, err_msg = building_lib_transport.get_routes({x=0,y=0,z=0}, "right")
+    assert(routes == nil)
+    assert(target_mapblock == nil)
+    assert(err_msg)
 
     callback()
 end)
