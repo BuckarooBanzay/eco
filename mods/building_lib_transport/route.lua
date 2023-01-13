0fdb3f0c3557b61a5852d8c7692eb6ae5c522742
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-01-13 14:03:39 +0100

    route spec

--- mods/building_lib_transport/route.lua
+++ mods/building_lib_transport/route.lua
@@ -1,14 +1,20 @@
 
--- name => route
+-- ${playername}/${routename} => route
 local routes = {}
 
 -- TODO: persist in mod_storage
 
-function building_lib_transport.create_route(name, route)
-    route.name = name
-    routes[name] = route
+local function format_key(playername, routename)
+    return playername .. "/" .. routename
 end
 
-function building_lib_transport.get_route(name)
-    return routes[name]
+function building_lib_transport.create_route(playername, routename, route)
+    route.playername = playername
+    route.routename = routename
+    route.key = format_key(playername, routename)
+    routes[route.key] = route
+end
+
+function building_lib_transport.get_route(playername, routename)
+    return routes[format_key(playername, routename)]
 end
--- mods/building_lib_transport/route.spec.lua
+++ mods/building_lib_transport/route.spec.lua
@@ -1,10 +1,19 @@
 
 mtt.register("route", function(callback)
-    building_lib_transport.create_route("my_route", {
-        -- TODO
+    local playername = "singleplayer"
+    local routename = "my_route"
+
+    building_lib_transport.create_route(playername, routename, {
+        points = {
+            {x=0, y=0, z=0},
+            {x=1, y=0, z=0, task="load"},
+            {x=2, y=0, z=0},
+            {x=3, y=0, z=0, task="unload"},
+            {x=4, y=0, z=0}
+        }
     })
 
-    local route = building_lib_transport.get_route("my_route")
+    local route = building_lib_transport.get_route(playername, routename)
     assert(route)
 
     callback();
