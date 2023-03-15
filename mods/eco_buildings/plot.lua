c568bc502ce257b8e9647c934bc3f989b300f56b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-15 09:56:28 +0100

    plot arrow wip

--- mods/eco_buildings/plot.lua
+++ mods/eco_buildings/plot.lua
@@ -1,9 +1,24 @@
 local MP = minetest.get_modpath("eco_buildings")
 
-local plots = {"1x1", "2x1", "2x2", "3x2", "3x3"}
+local plots = {
+    ["1x1"] = {
+        markers = {
+            {
+                texture = "building_lib_arrow.png",
+                position = {x=1.5, y=0.2, z=0.5},
+                rotation = {x=math.pi/2, y=0, z=math.pi/2},
+                size = {x=10, y=10}
+            }
+        }
+    },
+    ["2x1"] = {},
+    ["2x2"] = {},
+    ["3x2"] = {},
+    ["3x3"] = {}
+}
 
-for _, suffix in ipairs(plots) do
-    building_lib.register_building("eco_buildings:plot_" .. suffix, {
+for suffix, overrides in pairs(plots) do
+    local def = {
         catalog = MP .. "/schematics/plot_" .. suffix .. ".zip",
         conditions = {
             {
@@ -17,5 +32,11 @@ for _, suffix in ipairs(plots) do
             ["plot_" .. suffix] = true
         },
         overview = "eco:slab_stone_2"
-    })
+    }
+
+    for k, v in pairs(overrides) do
+        def[k] = v
+    end
+
+    building_lib.register_building("eco_buildings:plot_" .. suffix, def)
 end
