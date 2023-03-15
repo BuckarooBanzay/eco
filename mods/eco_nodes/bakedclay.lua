577cb2c74e864607e8538ea6b406447604fe17b7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-15 08:06:50 +0100

    baked clay cleanup

--- mods/eco_nodes/bakedclay.lua
+++ mods/eco_nodes/bakedclay.lua
@@ -1,6 +1,36 @@
 
-eco_nodes.register("baked_clay_white", {
+local bakedclay = {
+    ["white"] = {},
+    ["yellow"] = {},
+    ["black"] = {},
+    ["natural"] = {},
+    ["dark_grey"] = {},
+    ["dark_green"] = {},
+    ["pink"] = {},
+    ["grey"] = {},
+    ["magenta"] = {},
+    ["brown"] = {},
+    ["green"] = {},
+    ["cyan"] = {},
+    ["blue"] = {},
+    ["red"] = {},
+    ["violet"] = {},
+    ["orange"] = {}
+}
+
+-- separate color-nodes and full moreblocks support
+for name, def in pairs(bakedclay) do
+    def.tiles = def.tiles or {"baked_clay_" .. name .. ".png"}
+    def.sounds = def.sounds or eco_nodes.node_sound_stone()
+    def.moreblocks = true
+
+    eco_nodes.register("baked_clay_" .. name, def)
+end
+
+-- colorable and limited moreblocks support
+eco_nodes.register("baked_clay_colorable", {
     tiles = {"baked_clay_white.png"},
+    sounds = eco_nodes.node_sound_stone(),
     moreblocks = true,
     unifieddyes = true
 })
