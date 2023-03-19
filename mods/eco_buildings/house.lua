2fa2f7477eb899d6fdb724bf918f2046c71dd87f
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-19 20:15:04 +0100

    replacement randomizer wip

--- mods/eco_buildings/house.lua
+++ mods/eco_buildings/house.lua
@@ -5,19 +5,18 @@ local MP = minetest.get_modpath("eco_buildings")
 -- eco:slope_baked_clay_white_half
 -- eco:jungle_wood
 
-local replacements = {
-	{
-		["eco:slope_baked_clay_white_half_raised"] = "eco:slope_baked_clay_blue_half_raised",
-		["eco:slope_baked_clay_white_half"] = "eco:slope_baked_clay_blue_half",
-		["eco:baked_clay_white"] = "eco:baked_clay_blue",
-		["eco:jungle_wood"] = "eco:acacia_wood"
-	},{
-		["eco:slope_baked_clay_white_half_raised"] = "eco:slope_baked_clay_green_half_raised",
-		["eco:slope_baked_clay_white_half"] = "eco:slope_baked_clay_green_half",
-		["eco:baked_clay_white"] = "eco:baked_clay_green",
-		["eco:jungle_wood"] = "eco:oak_wood"
-	}
-}
+local function replacement_randomizer(replacement_map)
+	return function()
+		local replacements = {}
+		for src, choices in pairs(replacement_map) do
+			local new_nodename = choices[math.random(#choices)]
+			if src ~= new_nodename then
+				replacements[src] = new_nodename
+			end
+		end
+		return replacements
+	end
+end
 
 building_lib.register_building("eco_buildings:house_1", {
 	catalog = MP .. "/schematics/house_1.zip",
@@ -27,9 +26,28 @@ building_lib.register_building("eco_buildings:house_1", {
 			["underground"] = { group = "flat_surface"}
 		}
 	},
-	replace = function()
-		return replacements[math.random(#replacements)]
-	end,
+	replace = replacement_randomizer({
+		["eco:slope_baked_clay_white_half_raised"] = {
+			"eco:slope_baked_clay_white_half_raised",
+			"eco:slope_baked_clay_blue_half_raised",
+			"eco:slope_baked_clay_green_half_raised"
+		},
+		["eco:slope_baked_clay_white_half"] = {
+			"eco:slope_baked_clay_white_half",
+			"eco:slope_baked_clay_blue_half",
+			"eco:slope_baked_clay_green_half"
+		},
+		["eco:baked_clay_white"] = {
+			"eco:baked_clay_white",
+			"eco:baked_clay_green",
+			"eco:baked_clay_blue",
+			"eco:baked_clay_red"
+		},
+		["eco:jungle_wood"] = {
+			"eco:jungle_wood",
+			"eco:oak_wood"
+		}
+	}),
 	groups = {
 		house = true
 	},
