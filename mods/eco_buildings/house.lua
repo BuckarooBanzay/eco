e850e7f24f4a8d2b4f19c15567766da278a310de
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-19 21:14:21 +0100

    proper randomization

--- mods/eco_buildings/house.lua
+++ mods/eco_buildings/house.lua
@@ -8,7 +8,20 @@ local MP = minetest.get_modpath("eco_buildings")
 local function replacement_randomizer(replacement_map)
 	return function()
 		local replacements = {}
-		for src, choices in pairs(replacement_map) do
+
+		local sync_i
+		for src, choices in pairs(replacement_map.sync or {}) do
+			if not sync_i then
+				sync_i = math.random(#choices)
+			end
+
+			local new_nodename = choices[sync_i]
+			if src ~= new_nodename then
+				replacements[src] = new_nodename
+			end
+		end
+
+		for src, choices in pairs(replacement_map.random or {}) do
 			local new_nodename = choices[math.random(#choices)]
 			if src ~= new_nodename then
 				replacements[src] = new_nodename
@@ -27,25 +40,31 @@ building_lib.register_building("eco_buildings:house_1", {
 		}
 	},
 	replace = replacement_randomizer({
-		["eco:slope_baked_clay_white_half_raised"] = {
-			"eco:slope_baked_clay_white_half_raised",
-			"eco:slope_baked_clay_blue_half_raised",
-			"eco:slope_baked_clay_green_half_raised"
+		sync = {
+			["eco:slope_baked_clay_white_half_raised"] = {
+				"eco:slope_baked_clay_white_half_raised",
+				"eco:slope_baked_clay_blue_half_raised",
+				"eco:slope_baked_clay_green_half_raised",
+				"eco:slope_baked_clay_red_half_raised"
+			},
+			["eco:slope_baked_clay_white_half"] = {
+				"eco:slope_baked_clay_white_half",
+				"eco:slope_baked_clay_blue_half",
+				"eco:slope_baked_clay_green_half",
+				"eco:slope_baked_clay_red_half"
+			},
+			["eco:baked_clay_white"] = {
+				"eco:baked_clay_white",
+				"eco:baked_clay_blue",
+				"eco:baked_clay_green",
+				"eco:baked_clay_red"
+			},
 		},
-		["eco:slope_baked_clay_white_half"] = {
-			"eco:slope_baked_clay_white_half",
-			"eco:slope_baked_clay_blue_half",
-			"eco:slope_baked_clay_green_half"
-		},
-		["eco:baked_clay_white"] = {
-			"eco:baked_clay_white",
-			"eco:baked_clay_green",
-			"eco:baked_clay_blue",
-			"eco:baked_clay_red"
-		},
-		["eco:jungle_wood"] = {
-			"eco:jungle_wood",
-			"eco:oak_wood"
+		random = {
+			["eco:jungle_wood"] = {
+				"eco:jungle_wood",
+				"eco:oak_wood"
+			}
 		}
 	}),
 	groups = {
