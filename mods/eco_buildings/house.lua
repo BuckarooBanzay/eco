f2f5fa85b1cc7bd1923b3f6d73825b775f3c0947
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-03-19 20:04:59 +0100

    randomization

--- mods/eco_buildings/house.lua
+++ mods/eco_buildings/house.lua
@@ -1,5 +1,24 @@
 local MP = minetest.get_modpath("eco_buildings")
 
+-- eco:silver_sandstone_brick_colorable
+-- eco:slope_baked_clay_white_half_raised
+-- eco:slope_baked_clay_white_half
+-- eco:jungle_wood
+
+local replacements = {
+	{
+		["eco:slope_baked_clay_white_half_raised"] = "eco:slope_baked_clay_blue_half_raised",
+		["eco:slope_baked_clay_white_half"] = "eco:slope_baked_clay_blue_half",
+		["eco:baked_clay_white"] = "eco:baked_clay_blue",
+		["eco:jungle_wood"] = "eco:acacia_wood"
+	},{
+		["eco:slope_baked_clay_white_half_raised"] = "eco:slope_baked_clay_green_half_raised",
+		["eco:slope_baked_clay_white_half"] = "eco:slope_baked_clay_green_half",
+		["eco:baked_clay_white"] = "eco:baked_clay_green",
+		["eco:jungle_wood"] = "eco:oak_wood"
+	}
+}
+
 building_lib.register_building("eco_buildings:house_1", {
 	catalog = MP .. "/schematics/house_1.zip",
 	conditions = {
@@ -8,6 +27,9 @@ building_lib.register_building("eco_buildings:house_1", {
 			["underground"] = { group = "flat_surface"}
 		}
 	},
+	replace = function()
+		return replacements[math.random(#replacements)]
+	end,
 	groups = {
 		house = true
 	},
