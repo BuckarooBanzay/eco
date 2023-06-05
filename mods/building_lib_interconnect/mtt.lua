ba444dab05c36a26a8a0521baa4ba457827b316b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-06-05 11:09:47 +0200

    impl and test for `building_lib_interconnect.scan`

--- mods/building_lib_interconnect/mtt.lua
+++ mods/building_lib_interconnect/mtt.lua
@@ -3,8 +3,10 @@
 building_lib.register_building("test_ic:water_pump", {
     placement = "dummy",
     interconnect = {
-        producer = {
-            water = 200
+        connects_to = {
+            water = {
+                { x=1, y=0, z=0 }
+            }
         }
     }
 })
@@ -13,16 +15,21 @@ building_lib.register_building("test_ic:water_pipe", {
     placement = "dummy",
     interconnect = {
         connects = {
-            water = true
+            water = {
+                { x=1, y=0, z=0 },
+                { x=-1, y=0, z=0 }
+            }
         }
     }
 })
 
-building_lib.register_building("test_ic:water_distribution", {
+building_lib.register_building("test_ic:water_sink", {
     placement = "dummy",
     interconnect = {
-        consumer = {
-            water = 50
+        connects_to = {
+            water = {
+                { x=-1, y=0, z=0 }
+            }
         }
     }
 })
@@ -35,9 +42,16 @@ mtt.register("interconnect", function(callback)
 
     assert(building_lib.build({x=0,y=0,z=0}, playername, "test_ic:water_pump"))
     assert(building_lib.build({x=1,y=0,z=0}, playername, "test_ic:water_pipe"))
+    assert(building_lib.build({x=1,y=0,z=1}, playername, "test_ic:water_sink", 90)) -- rotated and not connected
     assert(building_lib.build({x=2,y=0,z=0}, playername, "test_ic:water_pipe"))
     assert(building_lib.build({x=3,y=0,z=0}, playername, "test_ic:water_pipe"))
-    assert(building_lib.build({x=4,y=0,z=0}, playername, "test_ic:water_distribution"))
+    assert(building_lib.build({x=3,y=0,z=1}, playername, "test_ic:water_sink")) -- not connected
+    assert(building_lib.build({x=4,y=0,z=0}, playername, "test_ic:water_sink"))
+
+    local pos_list = building_lib_interconnect.scan({x=1,y=0,z=0}, "water")
+    assert(#pos_list == 2) -- 1 pump and 1 sink
+    assert(vector.equals(pos_list[1], {x=4,y=0,z=0}))
+    assert(vector.equals(pos_list[2], {x=0,y=0,z=0}))
 
     callback()
 end)
---
+++ mods/building_lib_interconnect/store.lua
@@ -0,0 +1,6 @@
+
+local store = minetest.get_mod_storage()
+local data_store = mapblock_lib.create_data_storage(store)
+
+function building_lib_interconnect.get_network()
+end
