65cda1b15eea983e84dd8ce68fd0532f461d2384
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-04-24 16:37:05 +0200

    wip

--- mods/eco_transport/common.lua
+++ mods/eco_transport/common.lua
@@ -1,6 +1,5 @@
-local max = { x=15, y=15, z=15 }
 
-local function flip_point(axis, point)
+local function flip_point(axis, max, point)
     point[axis] = max[axis] - point[axis]
 end
 
@@ -8,25 +7,28 @@ local function transpose_point(axis1, axis2, point)
     point[axis1], point[axis2] = point[axis2], point[axis1]
 end
 
-local function rotate_point(point, rotation)
+-- rotates a single point
+local function rotate_point(point, max, rotation)
     if rotation == 90 then
-        flip_point("x", point)
+        flip_point("x", max, point)
         transpose_point("x", "z", point)
     elseif rotation == 180 then
-        flip_point("x", point)
-        flip_point("z", point)
+        flip_point("x", max, point)
+        flip_point("z", max, point)
     elseif rotation == 270 then
-        flip_point("z", point)
+        flip_point("z", max, point)
         transpose_point("x", "z", point)
     end
 end
 
-function eco_transport.rotate_routes(routes, rotation)
+-- rotates all the routes and returns a new route-set with given rotation
+function eco_transport.rotate_routes(routes, building_size, rotation)
     if rotation == 0 then
         -- no rotation
         return routes
     end
 
+    local max = vector.subtract( vector.multiply(building_size, 16), 1)
     local rotated_routes = {}
 
     for name, route in pairs(routes) do
@@ -37,7 +39,7 @@ function eco_transport.rotate_routes(routes, rotation)
         for _, point in ipairs(route.points) do
             -- copy point for modification
             point = table.copy(point)
-            rotate_point(point, rotation)
+            rotate_point(point, max, rotation)
             table.insert(rotated_route.points, point)
         end
 
@@ -45,4 +47,35 @@ function eco_transport.rotate_routes(routes, rotation)
     end
 
     return rotated_routes
+end
+
+-- returns the direction (in mapblocks) of the given route
+function eco_transport.get_connected_route_dir(route, building_size)
+    local max = vector.subtract( vector.multiply(building_size, 16), 0.5)
+    local min = { x=-0.5, y=-0.5, z=-0.5 }
+    local dir = { x=0, y=0, z=0 }
+
+    local last_point = route.points[#route.points]
+    for _, axis in ipairs({"x", "y", "z"}) do
+        if last_point[axis] == min[axis] then
+            dir[axis] = -1
+            break
+        elseif last_point[axis] == max[axis] then
+            dir[axis] = building_size[axis]
+            break
+        end
+    end
+
+    return dir
+end
+
+function eco_transport.find_connected_route(source_route, building_size, target_routes, target_offset)
+    -- TODO
+    local max = vector.subtract( vector.multiply(building_size, 16), 0.5)
+    local dir = eco_transport.get_connected_route_dir(source_route, building_size)
+
+    for name, route in pairs(target_routes) do
+    end
+
+    return {}
 end
--- mods/eco_transport/common.spec.lua
+++ mods/eco_transport/common.spec.lua
@@ -9,7 +9,10 @@ mtt.register("rotate_routes", function(callback)
             }
         }
     }
-    local routes_90deg = eco_transport.rotate_routes(routes, 90)
+
+    local building_size = { x=1, y=1, z=1 }
+
+    local routes_90deg = eco_transport.rotate_routes(routes, building_size, 90)
     assert(routes_90deg.main.type == "container-3")
     assert(routes_90deg.main.points[1].x == 0)
     assert(routes_90deg.main.points[1].y == 0)
@@ -17,5 +20,54 @@ mtt.register("rotate_routes", function(callback)
     assert(routes_90deg.main.points[2].x == 0)
     assert(routes_90deg.main.points[2].y == 0)
     assert(routes_90deg.main.points[2].z == -0.5)
+    callback()
+end)
+
+mtt.register("get_connected_route_dir", function(callback)
+    local route = {
+        points = {
+            { x=-0.5, y=0, z=0 },
+            { x=15.5, y=0, z=0 }
+        }
+    }
+
+    local building_size = { x=1, y=1, z=1 }
+
+    local dir = eco_transport.get_connected_route_dir(route, building_size)
+    assert(dir.x == 1)
+    assert(dir.y == 0)
+    assert(dir.z == 0)
+
+    callback()
+end)
+
+
+mtt.register("find_connected_route", function(callback)
+    local target_routes = {
+        main = {
+            type = "container-3",
+            points = {
+                { x=-0.5, y=0, z=0 },
+                { x=15.5, y=0, z=0 }
+            }
+        }
+    }
+
+    local target_offset = { x=1, y=0, z=0 }
+
+    local source_route = {
+        type = "container-3",
+        points = {
+            { x=-0.5, y=0, z=0 },
+            { x=15.5, y=0, z=0 }
+        }
+    }
+
+    local building_size = { x=1, y=1, z=1 }
+
+    local connected_route = eco_transport.find_connected_route(source_route, building_size, target_routes, target_offset)
+    assert(connected_route)
+    -- TODO
+
     callback()
 end)
