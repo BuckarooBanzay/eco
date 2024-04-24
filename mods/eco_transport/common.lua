a6880f18556735eb95e6bf190c108f8ea3fdc83d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-04-24 16:13:26 +0200

    route rotation

--- mods/eco_transport/common.lua
+++ mods/eco_transport/common.lua
@@ -1,4 +1,48 @@
+local max = { x=15, y=15, z=15 }
 
-function eco_transport.rotate_routes(route, rotation)
-    return {}
+local function flip_point(axis, point)
+    point[axis] = max[axis] - point[axis]
+end
+
+local function transpose_point(axis1, axis2, point)
+    point[axis1], point[axis2] = point[axis2], point[axis1]
+end
+
+local function rotate_point(point, rotation)
+    if rotation == 90 then
+        flip_point("x", point)
+        transpose_point("x", "z", point)
+    elseif rotation == 180 then
+        flip_point("x", point)
+        flip_point("z", point)
+    elseif rotation == 270 then
+        flip_point("z", point)
+        transpose_point("x", "z", point)
+    end
+end
+
+function eco_transport.rotate_routes(routes, rotation)
+    if rotation == 0 then
+        -- no rotation
+        return routes
+    end
+
+    local rotated_routes = {}
+
+    for name, route in pairs(routes) do
+        -- copy route and reset points field
+        local rotated_route = table.copy(route)
+        rotated_route.points = {}
+
+        for _, point in ipairs(route.points) do
+            -- copy point for modification
+            point = table.copy(point)
+            rotate_point(point, rotation)
+            table.insert(rotated_route.points, point)
+        end
+
+        rotated_routes[name] = rotated_route
+    end
+
+    return rotated_routes
 end
--- mods/eco_transport/common.spec.lua
+++ mods/eco_transport/common.spec.lua
@@ -1,7 +1,21 @@
 
 mtt.register("rotate_routes", function(callback)
-    local routes = {}
+    local routes = {
+        main = {
+            type = "container-3",
+            points = {
+                { x=-0.5, y=0, z=0 },
+                { x=15.5, y=0, z=0 }
+            }
+        }
+    }
     local routes_90deg = eco_transport.rotate_routes(routes, 90)
-    assert(routes_90deg) --TODO
+    assert(routes_90deg.main.type == "container-3")
+    assert(routes_90deg.main.points[1].x == 0)
+    assert(routes_90deg.main.points[1].y == 0)
+    assert(routes_90deg.main.points[1].z == 15.5)
+    assert(routes_90deg.main.points[2].x == 0)
+    assert(routes_90deg.main.points[2].y == 0)
+    assert(routes_90deg.main.points[2].z == -0.5)
     callback()
 end)
