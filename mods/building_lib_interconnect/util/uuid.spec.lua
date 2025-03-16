a164a85d65280b6758385f8e2ac19f3519e27388
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-03-16 21:25:40 +0100

    remove stuff

--- mods/building_lib_interconnect/util/uuid.spec.lua
+++
@@ -1,7 +0,0 @@
-
-
-mtt.register("uuid", function(callback)
-    assert(building_lib_interconnect.new_uuid())
-    assert(building_lib_interconnect.new_uuid() ~= building_lib_interconnect.new_uuid())
-    callback()
-end)
